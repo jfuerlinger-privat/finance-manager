fa92ecb3bc9906f627237691ad8fa13b789bd637
Josef Fürlinger <josef.fuerlinger@gmail.com>
2021-02-05 21:21:07 +0100

    read tags from api

--- frontend/simple-client/src/app.js
+++ frontend/simple-client/src/app.js
@@ -8,22 +8,41 @@ import '../node_modules/bootstrap-fileinput/js/fileinput';
 import '../node_modules/bootstrap-fileinput/themes/fas/theme';
 import '../node_modules/bootstrap-fileinput/js/locales/de';
 
+const axios = require('axios').default;
+
 // Stylesheets
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap-fileinput/css/fileinput.css';
 import './styles.css';
 
-const urlBaseLocal = 'http://localhost:7001';
+const urlBaseLocal = 'http://localhost:7071';
 const urlBaseCloud = 'https://func-financemanager-dev.azurewebsites.net';
 const urlUploadInvoices = 'api/invoice/upload';
 
-jQuery(() => {
+jQuery(async () => {
 
-    let action = `${urlBaseLocal}/${urlUploadInvoices}`;
+    let baseUrl = urlBaseLocal;
     if (document.location.hostname !== "localhost") {
-        action = `${urlBaseCloud}/${urlUploadInvoices}`;
+        baseUrl = urlBaseCloud;
     }
 
-    document.getElementById('frmInvoiceUpload').action = action;
+    axios.defaults.baseURL = baseUrl;
+
+    try {
+        let tags = (await axios.get('/api/tags')).data;
+        tags.map(tag => {
+            console.log(tag);
+            let select = document.getElementById('tags');
+            var option = document.createElement("option");
+            option.text = tag;
+            select.add(option);
+        });
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
+    document.getElementById('frmInvoiceUpload').action = `${baseUrl}/${urlUploadInvoices}`;
     console.log(document.getElementById('frmInvoiceUpload').action);
 });
